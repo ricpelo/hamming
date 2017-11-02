275481a2de06adc4dafac8c1608a4630c691d6f2
Ricardo Pérez <ricpelo@gmail.com>
2017-11-02 08:25:47 +0100

    Usa assertSame() en lugar de assertEquals()

--- test/HammingTest.php
+++ test/HammingTest.php
@@ -7,38 +7,37 @@ class HammingTest extends PHPUnit\Framework\TestCase
 
     public function testSinDiferenciaConCadenasIdenticas()
     {
-        $this->assertEquals(0, distancia('A', 'A'));
+        $this->assertSame(0, distancia('A', 'A'));
     }
 
-    public function testiConUnSoloCaracter()
+    public function testConUnSoloCaracter()
     {
-        $this->assertEquals(1, distancia('A', 'G'));
+        $this->assertSame(1, distancia('A', 'G'));
     }
 
     public function testConCadenasCortas()
     {
-        $this->assertEquals(2, distancia('AG', 'CT'));
+        $this->assertSame(2, distancia('AG', 'CT'));
     }
 
     public function testConCadenasLargas()
     {
-        $this->assertEquals(1, distancia('GGACG', 'GGTCG'));
+        $this->assertSame(1, distancia('GGACG', 'GGTCG'));
     }
 
     public function testConCadenasMasLargas()
     {
-        $this->assertEquals(4, distancia('GATACA', 'GCATAA'));
+        $this->assertSame(4, distancia('GATACA', 'GCATAA'));
     }
 
     public function testConCadenasMuchoMasLargas()
     {
-        $this->assertEquals(9, distancia('GGACGGATTCTG', 'AGGACGGATTCT'));
+        $this->assertSame(9, distancia('GGACGGATTCTG', 'AGGACGGATTCT'));
     }
 
-    public function testLanzaExcepcionConLongitudesDiferentes()
+    public function testDevuelveFalseConLongitudesDiferentes()
     {
-        $this->markTestSkipped();
-        $this->expectException('InvalidArgumentException', 'Las cadenas deben tener la misma longitud.');
-        distancia('GGACG', 'AGGACGTGG');
+        $this->assertSame(false, distancia('GGACG', 'AGGACGTGG'));
     }
 }
+
